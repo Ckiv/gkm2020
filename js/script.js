6b563001fbcf6b902dcca6325ed0ac5246e87115
Ckiv <alexandrshahok@gmail.com>
2020-02-11 16:40:55 +0200

    мелкие исправления

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 (function () {
     const menu_burger = document.querySelector('.menu_burger');
     const menu = document.querySelector('.menu');
-    const main_menu = document.querySelector('.main_menu');
     menu_burger.addEventListener('click', () => {
         menu_burger.classList.toggle('menu_burger_active');
         menu.style.display = "flex";
+        console.log("menu open");
     });
-    const menu_burger_active = document.querySelector('menu_burger_active');
-    menu_burger_active.addEventListener('click', () => {
-        menu.style.display = "none";
-        main_menu.style.height = "50px";
-    });
+    const menu_burger_active = document.querySelector('.menu_burger_active');
+    if (menu_burger_active){
+        menu_burger_active.addEventListener('click', () => {
+            menu.style.display = "none";
+            console.log("menu close");
+        });
+    }
 }());
