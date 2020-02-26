5e5b825987db5464f3e2604f8d40222443c36f83
Ckiv <alexandrshahok@gmail.com>
2020-02-26 15:40:19 +0200

    бургер меню работает

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
-(function () {
-    const menu_burger = document.querySelector('.menu_burger');
-    const menu = document.querySelector('.menu');
-    menu_burger.addEventListener('click', () => {
-        menu_burger.classList.toggle('menu_burger_active');
-        menu.style.display = "flex";
-        console.log("menu open");
-    });
-    const menu_burger_active = document.querySelector('.menu_burger_active');
-    if (menu_burger_active){
-        menu_burger_active.addEventListener('click', () => {
-            menu.style.display = "none";
-            console.log("menu close");
-        });
-    }
-}());
+document.getElementById("trigger").onclick = function() {
+    open()
+};
+
+function open() {
+    document.getElementById("menu").classList.toggle("show");
+}
